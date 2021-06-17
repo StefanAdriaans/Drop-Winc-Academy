5b4d1defbe5758f6c9e97306a7a90f2410f64425
Stefan Adriaans <81741082+StefanAdriaans@users.noreply.github.com>
2021-06-17 09:35:27 +0200

    Add files via upload

---
+++ 09_opdracht_Flexbox/_vti_cnf/script.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|15 Jun 2021 11:59:04 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|PC01\\Gebruiker
+vti_modifiedby:SR|PC01\\Gebruiker
+vti_timecreated:TR|10 Jun 2021 13:36:23 -0000
+vti_backlinkinfo:VX|Website\\ Winc\\ Academy/Winc_Academy/_Opdrachten/08_opdracht_Groter_HTML_Formulier/contact.html
+vti_cacheddtm:TX|15 Jun 2021 11:59:04 -0000
+vti_filesize:IR|297
