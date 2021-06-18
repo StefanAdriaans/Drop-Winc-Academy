72c6f5d1b70c034ec4a8953937f3e96efd13691c
Stefan Adriaans <81741082+StefanAdriaans@users.noreply.github.com>
2021-06-18 15:03:43 +0200

    Add files via upload

---
+++ 10_opdracht_Grid/_vti_cnf/index copy.php
@@ -0,0 +1,17 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|11 Jun 2021 18:22:44 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|PC01\\Gebruiker
+vti_modifiedby:SR|PC01\\Gebruiker
+vti_timecreated:TR|11 Jun 2021 18:22:44 -0000
+vti_cacheddtm:TX|11 Jun 2021 18:22:44 -0000
+vti_filesize:IR|521
+vti_cachedtitle:SR|Gegroet
+vti_cachedbodystyle:SR|<body>
+vti_cachedneedsrewrite:BR|false
+vti_cachedhasbots:BR|false
+vti_cachedhastheme:BR|false
+vti_cachedhasborder:BR|false
+vti_charset:SR|windows-1252
+vti_title:SR|Gegroet
+vti_backlinkinfo:VX|
